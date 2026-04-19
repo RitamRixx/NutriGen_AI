2bc6b29c9d08c326dbe19d9568cd30976583b6a2
Ritam Rakshit <ritamrakshit33@gmail.com>
2026-04-19 11:02:00 +0530

    testing: llms are working fine

---
+++ notebook/llm_testing.py
@@ -0,0 +1,22 @@
+from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+llm1 = ChatOpenAI(
+    model="openai/gpt-4o-mini",
+    openai_api_key=os.getenv("OPENROUTER_API_KEY"),
+    openai_api_base="https://openrouter.ai/api/v1",
+    temperature=0.7,
+)
+
+llm2 = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    google_api_key=os.getenv("GOOGLE_API_KEY")
+)
+
+result = llm2.invoke("What is the capital of France?")
+
+print(result.content)
